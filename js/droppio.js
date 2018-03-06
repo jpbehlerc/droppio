98d142f4488af68e6159a4132ce9ff4db2899efd
jpBehler <jpbehler@MacBook-de-Beatriz.local>
2018-03-06 20:20:01 -0300

    Added donate modal

--- js/droppio.js
+++ js/droppio.js
@@ -1,7 +1,6 @@
-
-  $(document).ready(function() {
-    $('select').material_select();
-    $('.datepicker').pickadate({
+$(document).ready(function() {
+  $('select').material_select();
+  $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
     today: 'Today',
@@ -9,4 +8,31 @@
     close: 'Ok',
     closeOnSelect: false // Close upon selecting a date,
   })
+
+  $('.donate').click(function(e) {
+
+    swal({
+      html: '<h6>Testing</h6>',
+      showCancelButton: true,
+      confirmButtonText: 'Confirmar',
+      cancelButtonText: 'Cancelar'
+    }).then(function(result) {
+      if (result.value) {
+        swal(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+        )
+        // For more information about handling dismissals please visit
+        // https://sweetalert2.github.io/#handling-dismissals
+      } else if (result.dismiss === swal.DismissReason.cancel) {
+        swal(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    });
+
   });
+});
