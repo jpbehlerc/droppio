9999be5fe00c755cf29b830a429b605c77a23c9b
Francisco Innocenti <franciscoinnocenti@Franciscos-MacBook-Pro.local>
2018-03-13 02:28:09 -0300

    si si

--- js/website.js
+++ js/website.js
@@ -1,39 +1,8 @@
-// countDown
-const second = 1000,
-minute = second * 60,
-hour = minute * 60,
-day = hour * 24;
-
-let countDown = new Date('april 29, 2018 00:00:00').getTime(),
-x = setInterval(function() {
-
-  let now = new Date().getTime(),
-  distance = countDown - now;
-
-  document.getElementById('days').innerHTML = Math.floor(distance / (day)),
-  document.getElementById('hours').innerHTML = Math.floor((distance % (day)) / (hour)),
-  document.getElementById('minutes').innerHTML = Math.floor((distance % (hour)) / (minute)),
-  document.getElementById('seconds').innerHTML = Math.floor((distance % (minute)) / second);
-
-  //do something later when date is reached
-  //if (distance < 0) {
-  //  clearInterval(x);
-  //  'IT'S MY BIRTHDAY!;
-  //}
-
-}, second)
-
-// Scroolfire
-var options = [
-  {selector: '.scrollFire', offset: 200, callback: function (el) {
-    Materialize.fadeInImage($(el));
-  }
-}];
-Materialize.scrollFire(options);
-
 
 // init
 $(document).ready(function(){
+
+  $('select').material_select();
   $('.parallax').parallax();
   // Scrolly
   //this is where we apply opacity to the arrow
@@ -51,7 +20,5 @@ $(document).ready(function(){
     var position = topWindow / windowHeight;
     //invert the percentage
     position = 1 - position;
-
-
   });
-})
+});
