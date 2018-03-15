049992e98012c83f951af824ba4c75f36302e85c
Francisco Innocenti <franciscoinnocenti@Franciscos-MacBook-Pro.local>
2018-03-15 00:12:00 -0300

    to closeee

--- js/website.js
+++ js/website.js
@@ -3,37 +3,19 @@ $(document).ready(function() {
 
   $('select').material_select();
   $('.parallax').parallax();
-   $('.collapsible').collapsible();
-  // Scrolly
-  //this is where we apply opacity to the arrow
-  $(window).scroll(function() {
-
-    //get scroll position
-    var topWindow = $(window).scrollTop();
-    //multipl by 1.5 so the arrow will become transparent half-way up the page
-    var topWindow = topWindow * 1.5;
-
-    //get height of window
-    var windowHeight = $(window).height();
-
-    //set position as percentage of how far the user has scrolled
-    var position = topWindow / windowHeight;
-    //invert the percentage
-    position = 1 - position;
-  });
+  $('.collapsible').collapsible();
+  $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
 });
-
-var options = [{
-  selector: '.fadeInImage1',
-  offset: 300,
-  callback: function(el) {
-    Materialize.fadeInImage($(el));
-  }
-}, {
-  selector: '.fadeInImage2',
-  offset: 500,
-  callback: function(el) {
-    Materialize.fadeInImage($(el));
-  }
-}];
-Materialize.scrollFire(options);
+window.onload = function() {
+  scrolly();
+};
