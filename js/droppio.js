2982a90aa471c4ad5e794aa38fa32dcccf96782c
Francisco Innocenti <franciscoinnocenti@Franciscos-MacBook-Pro.local>
2018-03-19 18:37:37 -0300

    some corrections have been fixed

--- js/droppio.js
+++ js/droppio.js
@@ -22,6 +22,17 @@ $(document).ready(function() {
     close: 'Cerrar'
   });
 
+  $('.timepicker').pickatime({
+    default: false, // Set default time: 'now', '1:30AM', '16:30'
+    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+    twelvehour: true, // Use AM/PM or 24-hour format
+    donetext: 'OK', // text for done-button
+    cleartext: 'Borrar', // text for clear-button
+    canceltext: 'Cancelar', // Text for cancel-button
+    autoclose: true, // automatic close timepicker
+    ampmclickable: true, // make AM PM clickable
+    aftershow: function(){} //Function for after opening timepicker
+  });
   $('select').material_select();
   // Success Modal
   $('.spread').click(function(e) {
