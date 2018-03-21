3593a7e2bd0bfce38b7c736ddcd2d68fe1a393f1
jpBehler <jpbehler@MacBook-de-Beatriz.local>
2018-03-21 19:37:16 -0300

    Starting pouchDBs functionality

--- js/droppioHome.js
+++ js/droppioHome.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
 
-
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
+  //Init respective DBs
+  var campaigns = new PouchDB("campaigns_userID", {
+    auto_compaction: false,
+    cache: false,
+    heartbeat: true
   });
 
+  var settings = new PouchDB("settings_userID", {
+    auto_compaction: false,
+    cache: false,
+    heartbeat: true
+  });
 
-  //$('#pickadate').pickadate();
-  $('select').material_select();
+  var remote_campaigns = new PouchDB('https://droppio.org:6489/campaigns');
+  var remote_settings = new PouchDB('https://droppio.org:6489/settings_userID');
 
-
+  //Register campaign events
   $('.spread').click(function(e) {
 
     swal('Gracias!',
@@ -22,6 +24,7 @@ $(document).ready(function() {
       'success');
 
   });
+
   $('.donate').click(function(e) {
 
     swal({
@@ -61,4 +64,77 @@ $(document).ready(function() {
     });
 
   });
+
+  //Sync settings only then start campaign filtration
+  settings.sync(remote_settings, {
+
+    live: true,
+    retry: true,
+    back_off_function: function(delay) {
+
+      if (delay == 0) {
+
+        return 1000;
+
+      } else if (delay >= 1000 && delay < 1800000) {
+
+        return delay * 1.5;
+
+      } else if (delay >= 1800000) {
+
+        return delay * 1.1;
+
+      }
+
+    }
+  }).on('paused', function(err) {
+
+    settings.get('bloodType').then(function(doc) {
+
+      var bloodType = doc.type;
+
+      campaigns.replicate.from(remote_campaigns, {
+
+        live: true,
+        retry: true,
+        back_off_function: function(delay) {
+
+          if (delay == 0) {
+
+            return 1000;
+
+          } else if (delay >= 1000 && delay < 1800000) {
+
+            return delay * 1.5;
+
+          } else if (delay >= 1800000) {
+
+            return delay * 1.1;
+
+          }
+        },
+        selector: {
+          "compatible": {
+            "$elemMatch": {
+              "$eq": bloodType
+            }
+          }
+
+        }
+
+      }).on('error', function(info) {
+        // I'll be back
+      });
+
+    });
+
+
+  }).on('error', function(info) {
+
+    console.log("Oops smth happened while trying to sync settings!");
+
+  });
+
+
+
 });
