0135690d1b971682d11b47084822bcfc79cef956
jpBehler <jpbehler@MacBookeBeatriz.lan>
2018-03-23 21:39:07 -0300

    Advanced on profile storage

--- js/droppioProfile.js
+++ js/droppioProfile.js
@@ -1,5 +1,65 @@
 $(document).ready(function() {
 
+  //Init pouchDB
+  function Settings() {
+
+    this.jsonified = [];
+    this.bloodType = false,
+      this.name = false,
+      this.lastname = false,
+      this.dni = false,
+      this.email = false,
+      this.weight = false,
+      this.birthDate = false,
+      this.password = false,
+      this.toJSON = function() {
+
+        for (var prop in this) {
+          if (this[prop].length)
+            this.jsonified.push({
+              '_id': prop,
+              value: this[prop]
+            });
+        }
+        return this.jsonified;
+      }
+  }
+
+  function getCookie(name) {
+    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    return r ? r[1] : false;
+  }
+
+  var info = new Settings();
+
+  $.post("/", {
+
+    _xsrf: getCookie('_xsrf'),
+    type: "creds"
+
+  }).done(function(data) {
+
+    data = JSON.parse(data);
+
+    respType = data['type'];
+
+    if (respType == 'creds') {
+
+      var dbUser = data['dbUser'];
+      var dbPass = data['dbPass'];
+
+      var settingsDB = new PouchDB("settings" + dbUser, {
+        auto_compaction: false,
+        cache: false,
+        heartbeat: true
+      });
+
+      var remote_settingssDB = new PouchDB('https://' + dbUser + ':' + dbPass + '@droppio.org:6489/settings' + dbUser);
+
+    }
+
+  });
+
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
@@ -12,4 +72,56 @@ $(document).ready(function() {
   //$('#pickadate').pickadate();
   $('select').material_select();
 
+  settings.sync(remote_settings, {
+
+    live: true,
+    retry: true,
+    back_off_function: function(delay) {
+
+      if (delay == 0) {
+
+        return 1000;
+
+      } else if (delay >= 1000 && delay < 1800000) {
+
+        return delay * 1.5;
+
+      } else if (delay >= 1800000) {
+
+        return delay * 1.1;
+
+      }
+
+    }
+
+  }).on('paused', function(err) {
+
+    $("#saveSettings").submit(function(e) {
+
+      info.name = $("#name").val();
+      info.lastname = $("#lastname").val();
+      info.bloodType = $("#bloodType").val();
+      info.dni = $("#dni").val();
+      info.email = $("#email").val();
+      info.birthDate = $("#birthDate").val();
+      info.weight = $("#weight").val();
+      info.password = $("#password").val();
+
+      for (var prop in info) {
+
+        settingsDB.get(prop).then(function(doc) {
+
+          doc.value = info[prop];
+
+          settingsDB.put(doc).catch(function(err) {
+            //Strong presence of the dark force I see here (show warning)
+          });
+
+        });
+      }
+    });
+
+
+
+  });
 });
