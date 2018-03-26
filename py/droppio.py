d64125076a5d3c57af4697df752392c626337a9d
jpBehler <jpbehler@MacBook-de-Beatriz.local>
2018-03-26 16:45:37 -0300

    Fixing login

--- py/droppio.py
+++ py/droppio.py
@@ -120,8 +120,7 @@ class landing(tornado.web.RequestHandler):
 class register(tornado.web.RequestHandler):
 
     def get(self):
-
-        self.xsrf_token()
+        
         self.render("signup.html")
 
     async def post(self):
