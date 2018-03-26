e3109ab6dad12fb969c464e29de7954b073237c1
Francisco Innocenti <franciscoinnocenti@Franciscos-MacBook-Pro.local>
2018-03-26 20:51:24 -0300

    birthdate

--- js/profile/droppioProfile.js
+++ js/profile/droppioProfile.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
       this.dni = false,
       this.email = false,
       this.weight = false,
-      this.birthday = false,
+      this.birthdate = false,
       this.password = false,
       this.toJSON = function() {
 
