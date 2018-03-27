c2c1c31badb8172b712d7b102fb7b0228e33a2cb
jpBehler <jpbehler@MacBook-de-Beatriz.local>
2018-03-26 21:18:28 -0300

    Fixed token error on droopio.py

--- py/droppio.py
+++ py/droppio.py
@@ -185,9 +185,7 @@ class register(tornado.web.RequestHandler):
 
             if name and lastname and bloodType and email:
 
-                print(email.encode())
-                
-                token = sha224(email.decode()).hexdigest()
+                token = sha224(email.encode()).hexdigest()
 
                 dbUser = 'droppio%s'%token
                 dbPass = "%s%s"%(token,self.settings['salt'])
