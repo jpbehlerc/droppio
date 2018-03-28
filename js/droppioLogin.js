c5e7eae0cf24bc19bc605fc2b88e737f0310727e
jpbehlerc <jpbehler@nabla.tech>
2018-03-28 17:42:04 -0300

    Almost there!

--- js/droppioLogin.js
+++ js/droppioLogin.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
       this.name = false,
       this.lastname = false,
       this.email = false,
+      this.email2 = false,
       this.password = false,
       this.type = false,
 
@@ -47,9 +48,10 @@ $(document).ready(function() {
     signx.type = $(this).attr("id");
     signx.name = $("#name").val();
     signx.lastname = $("#lastname").val();
-    console.log($("#bloodType").val());
+
     signx.bloodType = $("#bloodType").val();
     signx.email = $("#email").val();
+    signx.email2 = $("#email2").val();
     signx.password = $("#password").val();
     signx._xsrf = xsrf_token;
 
