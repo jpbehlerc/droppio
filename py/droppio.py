06f60392c48876960ea686a88d462021a9fd189b
jpbehlerc <jpbehler@nabla.tech>
2018-03-30 04:51:32 -0300

    Added missing py

--- py/droppio.py
+++ py/droppio.py
@@ -352,6 +352,7 @@ class register(tornado.web.RequestHandler):
         requestType = self.get_argument('type',default=False)
         requestType = requestType if requestType == 'creds' else False
 
+
         user,admin = tornado.escape.xhtml_escape(self.current_user).split("&")
 
         user = user.split(':')
