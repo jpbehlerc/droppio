d5a511daa27c6f912ed1f63196636568d4578fdf
jpbehlerc <jpbehler@nabla.tech>
2018-04-06 05:15:44 -0300

    Show JSONified settings

--- js/profile/droppioProfile.js
+++ js/profile/droppioProfile.js
@@ -129,7 +129,7 @@ $(document).ready(function() {
 
         elems = info.toJSON();
         keys = Object.keys(elems);
-
+        console.log(elems);
         if ('province' in elems) {
 
           hospitalsDB.find({
