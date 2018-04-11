5de121778a884c9ca8e59825e9803769827391dd
jpbehlerc <jpbehler@nabla.tech>
2018-04-10 23:26:57 -0300

    Filtering?

--- js/home/droppioHome.js
+++ js/home/droppioHome.js
@@ -155,7 +155,7 @@ $(document).ready(function() {
 
 
             }).on('change', function(docs) {
-              //New shiny card!
+              console.log(docs);
             });
 
           }
