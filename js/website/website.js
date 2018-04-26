f20ab27adda509b1c1fe93c16ce26cffcbf3607f
Francisco Innocenti <franciscoinnocenti@fmbp.local>
2018-04-26 20:31:54 -0300

    Debug register

--- js/website/website.js
+++ js/website/website.js
@@ -1,6 +1,6 @@
 // init
 $(document).ready(function() {
-
+  $('.modal').modal();
   $('select').formSelect();
   $('.parallax').parallax();
   $('.collapsible').collapsible();
@@ -8,17 +8,25 @@ $(document).ready(function() {
     $(this).find('.material-icons').toggleClass('rotate180');
   });
   $("a").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-                window.location.hash = hash;
-            });
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+        window.location.hash = hash;
+      });
         }
     });
+$('#register').submit(function() {
+  var name = $('#name').val();
+  var lastname = $('#lastname').val();
+  var bloodType = $('#bloodType').val();
+  var dni = $('#dni').val();
+  var email = $('#email').val();
+  var phone = $('#phone').val();
+  console.log(phone);
+  $('#confirmSignUp').find('#confirmText').html('<p><b>Nombre:</b> ' + name + '</p>' + '<p><b>Apellido:</b> ' + lastname + '</p>' + '<p><b>Tipo de Sangre:</b> ' + bloodType + '</p>' + '<p><b>Dni:</b> ' + dni + '</p>' + '<p><b>Email:</b> ' + email + '</p>' + '<p><b>Telefono:</b> ' + phone + '</p>') ;
+  });
+
 });
-window.onload = function() {
-  scrolly();
-};
