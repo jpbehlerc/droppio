d35bf885a3b6b2e3dd187c0a7cb23c27d00345b2
Francisco Innocenti <franciscoinnocenti@fmbp.local>
2018-04-30 19:34:49 -0300

    fix bug js

--- js/website/website.js
+++ js/website/website.js
@@ -22,40 +22,19 @@ $(document).ready(function() {
     data.email = $('#email').val();
     data.phone = $('#phone').val();
     console.log('alles gut');
-    switch (bloodTypeForm) {
-      case 1:
-        bloodTypeForm = "A+";
-        break;
-      case 2:
-        bloodTypeForm = "B+";
-        break;
-      case 3:
-        bloodTypeForm = "AB+";
-        break;
-      case 4:
-        bloodTypeForm = "O+";
-        break;
-      case 5:
-        bloodTypeForm = "A-";
-        break;
-      case 6:
-        bloodTypeForm = "B-";
-        break;
-      case 7:
-        bloodTypeForm = "AB-";
-        break;
-      case 8:
-        bloodTypeForm = "O-";
-        break;
-    };
-    $('#confirmSignUp').find('#confirmText').html('<p><b>Nombre:</b> ' + data.name + '</p>' + '<p><b>Apellido:</b> ' + data.lastname + '</p>' + '<p><b>Tipo de Sangre:</b> ' + data.bloodTypeForm + '</p>' + '<p><b>Dni:</b> ' + data.dni + '</p>' + '<p><b>Email:</b> ' + data.email + '</p>' + '<p><b>Telefono:</b> ' + data.phone + '</p>');
+    btype = {1:'A+',2:'B+',3:'AB+',4:'O+',5:'A-',6:'B-',7:'AB-',8:'O-'};
+    $('#confirmSignUp').find('#confirmText').html('<p><b>Nombre:</b> ' + data.name + '</p>' + '<p><b>Apellido:</b> ' + data.lastname + '</p>' + '<p><b>Tipo de Sangre:</b> ' + btype[data.bloodType] + '</p>' + '<p><b>Dni:</b> ' + data.dni + '</p>' + '<p><b>Email:</b> ' + data.email + '</p>' + '<p><b>Telefono:</b> ' + data.phone + '</p>');
+
     $('#confirmationButton').on('click', function() {
-      registerDB.post(data).then(function(result) {
+
+      registerDB.put(data).then(function(result) {
         //Escribio en la DB local con exito (la replicacion es automatica)
       }).catch(function(err) {
         //Algo raro paso al guardar en la DB local
       });
+
     });
+
   });
   $("a").on('click', function(event) {
     if (this.hash !== "") {
