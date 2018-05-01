47e56a49d601f1f0000032f23a50c3591eb54edd
jpbehlerc <jpbehler@nabla.tech>
2018-04-30 23:54:15 -0300

    Trying..

--- js/home/droppioHome.js
+++ js/home/droppioHome.js
@@ -154,8 +154,8 @@ $(document).ready(function() {
                   return delay * 1.1;
 
                 }
-              },
-
+              }
+              /*,
               filter: function(doc, req) {
 
                 var isCompatible = doc.compatible.includes(req.query.bloodType);
@@ -171,7 +171,7 @@ $(document).ready(function() {
                 nearbyHospitals: settings.nearbyHospitals,
                 expiry: moment().tz("America/Argentina/Buenos_Aires").subtract(30, 'days').valueOf()
               }
-
+              */
             }).on('change', function(change) {
 
               console.log(doc);
