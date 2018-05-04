4869ac8ecee143c082aca425be201721f5c2c8d9
Francisco Innocenti <franciscoinnocenti@fmbp.local>
2018-05-04 19:50:57 -0300

    fix merge

--- js/website/website.js
+++ js/website/website.js
@@ -3,11 +3,9 @@ $(document).ready(function() {
   $('select').formSelect();
   $('.parallax').parallax();
   $('.collapsible').collapsible();
-  $('.collapsible').on('click', function() {
+  $('.modal').modal();
+  $('.collapsible').on('click', '.collapsible-header', function() {
     $(this).find('.material-icons').first().toggleClass('rotate180');
-  });
-  $('.checkbox').click('input[checkbox]', function() {
-    $('.modal').modal();
   });
   $('#register').find('button').click(function() {
     console.log('gut');
@@ -16,12 +14,12 @@ $(document).ready(function() {
     data.lastname = $('#lastname').val();
     data.bloodType = $('#bloodType').val();
     var bloodTypeForm = $('#bloodType').val();;
+    btype = {1:'A+',2:'B+',3:'AB+',4:'O+',5:'A-',6:'B-',7:'AB-',8:'O-'};
     data.dni = $('#dni').val();
     data.email = $('#email').val();
     data.phone = $('#phone').val();
     console.log('alles gut');
-    btype = {1:'A+',2:'B+',3:'AB+',4:'O+',5:'A-',6:'B-',7:'AB-',8:'O-'};
-    $('#confirmSignUp').find('#confirmText').html('<p><b>Nombre:</b> ' + data.name + '</p>' + '<p><b>Apellido:</b> ' + data.lastname + '</p>' + '<p><b>Tipo de Sangre:</b> ' + btype[data.bloodType] + '</p>' + '<p><b>Dni:</b> ' + data.dni + '</p>' + '<p><b>Email:</b> ' + data.email + '</p>' + '<p><b>Telefono:</b> ' + data.phone + '</p>');
+    $('#confirmSignUp').find('#confirmText').html('<p><b>Nombre:</b> ' + data.name + '</p>' + '<p><b>Apellido:</b> ' + data.lastname + '</p>' + '<p><b>Tipo de Sangre:</b> ' + btype[data.bloodType] + '<p><b>Email:</b> ' + data.email + '</p>');
 
     $('#confirmationButton').on('click', function() {
 
